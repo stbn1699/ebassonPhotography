38b13da2aaf2e0fe0b0681d7dad456b45ee445a1
Plexi <stbnbasson@gmail.com>
2025-04-09 09:51:33 +0200

    feat: ajout d'un chevron sur la Home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,14 @@
-import './Home.scss';
+import "./Home.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
 	return (
 		<div className="home-container">
 			<img src="src/Images/logoCentralBlanc.png" alt=""/>
+			<div className="chevron-icon">
+				<FontAwesomeIcon icon={faChevronDown}/>
+			</div>
 		</div>
 	);
 }
