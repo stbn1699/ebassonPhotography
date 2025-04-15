c7211a4677f076a37e3b6dbf51373648d44d9041
Swan <stbnbasson@gmail.com>
2025-04-15 22:55:12 +0200

    temporary contact page nul a chier on va refaire ça hein

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -5,6 +5,22 @@ function Contact() {
 		<div className="contact">
 			<div className="title">Me Contacter</div>
 			<div className="content">
+				<div className="titres">
+					<div className="mail">
+						Soit par mail...
+					</div>
+					<div className="autres">
+						...Ou sur mes réseaux sociaux !
+					</div>
+				</div>
+				<div className="corps">
+					<div className="mail">
+						<a href="">Instagram</a>
+					</div>
+					<div className="autres">
+						<a href="">Instagram</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
